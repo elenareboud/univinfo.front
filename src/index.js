59a5f20a695936330853c803fee7e000d9d599ee
elenareboud <elena.reboud@gmail.com>
2024-02-27 17:23:16 +0100

    deploy3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import { App } from './components/app';
 
-// const rootDOMElement = document.getElementById('app');
-// const root = createRoot(rootDOMElement);
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('app'));
 root.render(<App />);
